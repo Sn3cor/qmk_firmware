4c8cf804a306045369b7cb154e865877f66b0a40
Sn3cor <jakub.krupa23@gmail.com>
2025-02-10 16:44:14 +0100

    new branch + dumbpad update

--- keyboards/dumbpad/v3x/keymaps/custom/keymap.c
+++ keyboards/dumbpad/v3x/keymaps/custom/keymap.c
@@ -78,4 +78,38 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             break;
     }
     return false;
-}
+
+    // }
+    // enum combos {
+    //     C1_DEL,
+    //     C2_END,
+    //     C3_PGDN,
+    //     C4_INSERT,
+    //     C5_HOME,
+    //     C6_PGUP,
+    //     C7_PRTSC,
+    //     C8_SCROLL,
+    //     C9_PAUSE
+    // };
+    // //Combos
+    // const uint16_t PROGMEM del[] = {KC_LCTL, KC_P1, COMBO_END};
+    // const uint16_t PROGMEM end[] = {KC_LCTL, KC_P2, COMBO_END};
+    // const uint16_t PROGMEM pgdn[] = {KC_LCTL, KC_P3, COMBO_END};
+    // const uint16_t PROGMEM insert[] = {KC_LCTL, KC_P4, COMBO_END};
+    // const uint16_t PROGMEM home[] = {KC_LCTL, KC_P5, COMBO_END};
+    // const uint16_t PROGMEM pgup[] = {KC_LCTL, KC_P6, COMBO_END};
+    // const uint16_t PROGMEM prtsc[] = {KC_LCTL, KC_P7, COMBO_END};
+    // const uint16_t PROGMEM scroll[] = {KC_LCTL, KC_P8, COMBO_END};
+    // const uint16_t PROGMEM pause[] = {KC_LCTL, KC_P9, COMBO_END};
+
+    // combo_t key_combos[] = {
+    //     [C1_DEL] = COMBO(del, KC_DEL),
+    //     [C2_END] = COMBO(end, KC_END),
+    //     [C3_PGDN] = COMBO(pgdn, KC_PGDN),
+    //     [C4_INSERT] = COMBO(insert, KC_INS),
+    //     [C5_HOME] = COMBO(home, KC_HOME),
+    //     [C6_PGUP] = COMBO(pgup, KC_PGUP),
+    //     [C7_PRTSC] = COMBO(prtsc, KC_PSCR),
+    //     [C8_SCROLL] = COMBO(scroll, KC_SCRL),
+    //     [C9_PAUSE] = COMBO(pause, KC_PAUSE)
+    // };
